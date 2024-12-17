d6cf554da8caf9ab836e6be14f93141ec38e8bc1
lespodsay <lespodsay@gmail.com>
2024-12-17 19:45:22 +0300

    Added Stack Screen to layout.jsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,7 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen 
       name="index" 
-      options={{headerShown: false}}
-      />
+      options={{headerShown: false}}/>
     </Stack>
   );
 }
