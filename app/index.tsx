a1c53ca7a48110276abb97a45a096af84eee9b6c
lespodsay <lespodsay@gmail.com>
2024-12-17 19:32:43 +0300

    Created Test App and installed some depenencies

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>bas ana ma kontesh motakhayel inik tesebini ya farida!! aa!</Text>
+    </View>
+  );
+}
